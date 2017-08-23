06b8d10eefedd680712ba287321dcde6034332c0
Jimmy Coral <jimmycoral@hotmail.com>
2017-08-23 16:40:54 -0500

    getting contact ai info on dashboard page

---
+++ ContactFacets.POC/Pipelines/Dashboard/QueryLatestVisitorsFromSearch.cs
@@ -0,0 +1,26 @@
+﻿using ContactFacets.POC.Models;
+using ContactFacets.POC.MongoDB;
+using Sitecore.Cintel.Reporting;
+using Sitecore.Cintel.Reporting.Processors;
+using System.Data;
+
+namespace ContactFacets.POC.Pipelines.Dashboard
+{
+    public class QueryLatestVisitorsFromSearch : ReportProcessorBase
+    {
+        public override void Process(ReportProcessorArgs args)
+        {
+            args.ResultTableForView.Columns.Add(new ViewField<string>("AITraining").ToColumn());
+            foreach(DataRow row in args.ResultTableForView.Rows)
+            {
+                row["AITraining"] = GetTrainingValue(row["ContactId"].ToString());
+            }
+        }
+
+        private string GetTrainingValue(string id)
+        {
+            return MongoDAO.GetContactAIDetails(id);
+           
+        }
+    }
+}
