4dd03830587c2db54d63362b5ceed20b06a06b55
Jimmy Coral <jimmycoral@hotmail.com>
2017-09-14 18:08:17 -0500

    Includes IoC in web api project

--- SitecoreAI.WebApi/App_Start/WebApiConfig.cs
+++ SitecoreAI.WebApi/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SitecoreAI.WebApi.IoC;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -10,6 +11,7 @@ namespace SitecoreAI.WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            Configurator.Register(config);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ SitecoreAI.WebApi/IoC/UnityDependencyResolver.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Practices.Unity;
+using System.Web.Http.Dependencies;
+
+namespace SitecoreAI.WebApi.IoC
+{
+    public class UnityDependencyResolver : IDependencyResolver
+    {
+        protected IUnityContainer container;
+
+        public UnityDependencyResolver(IUnityContainer container)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+            this.container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return new List<object>();
+            }
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = container.CreateChildContainer();
+            return new UnityDependencyResolver(child);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            container.Dispose();
+        }
+    }
+}
