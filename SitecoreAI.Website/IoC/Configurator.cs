493ce0095374be2b79e8b4c91bff0843ba63c671
Jimmy Coral <jimmycoral@hotmail.com>
2017-09-14 16:37:44 -0500

    IoC on Website project

---
+++ SitecoreAI.Website/IoC/Configurator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using SitecoreAI.MongoDB;
+
+namespace SitecoreAI.Website.IoC
+{
+    public class Configurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {            
+            serviceCollection.AddScoped(typeof(IContacts), typeof(ContactDAO));            
+            serviceCollection.AddMvcControllersInCurrentAssembly();
+        }
+    }
+}
